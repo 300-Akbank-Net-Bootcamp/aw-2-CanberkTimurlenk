d2a81447b3de88320b90c9e56917fbaa454b66d5
Canberk TİMURLENK <canberk.timurlenk@gmail.com>
2023-12-25 03:27:27 +0300

    Refactor PutAsync

--- Vb.Api/Controllers/AccountsController.cs
+++ Vb.Api/Controllers/AccountsController.cs
@@ -53,13 +53,17 @@ namespace VbApi.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> PutAsync(int id, Account account)
         {
+            // To follow the common conventions, I added the id to the route even though it is in the request body
+            // Request/Response Model could be used to overcome that, In that case account no more include the id
+
             if (id != account.Id)
                 return BadRequest();
             // returns 400 Bad Request if account id does not match with the id in the route
 
-            var entityToUpdate = await _context.Accounts.FindAsync(id);
+            var entityExists = (await _context.Accounts.AnyAsync(a => a.Id.Equals(id)));
+            // Since request/response models were not used, I checked if the entity exists manually.
 
-            if (entityToUpdate == null)
+            if (!entityExists)
                 return NotFound(); // returns 404 Not Found if not found
 
             _context.Accounts.Update(account);
